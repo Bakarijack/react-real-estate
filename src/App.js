077f2baa9819c8f2f6c3710f6d937ff856633a9e
Bakarijack <bakarikilu254@gmail.com>
2022-12-20 09:48:58 +0300

    work section ok

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Fragment } from 'react';
 import Home from './components/Home';
 import Property from './components/Property';
 import Services from './components/Services';
+import Work from './components/Work';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Home/>
       <Property/>
       <Services/>
+      <Work/>
     </Fragment>
   );
 }
---
+++ src/details/WorkDetail.js
@@ -0,0 +1,25 @@
+const WorkDetail = [
+    {
+        id: "1",
+        number: "01",
+        heading: "All Cash Offer",
+        info: "A small river name Tana"
+    },{
+        id: "2",
+        number: "02",
+        heading: "Meet Your Agent",
+        info: "A small river name Tana"
+    },{
+        id: "3",
+        number: "03",
+        heading: "Close the Deal",
+        info: "A small river name Tana"
+    },{
+        id: "4",
+        number: "04",
+        heading: "Have your Property",
+        info: "A small river name Tana"
+    },
+];
+
+export default WorkDetail;
